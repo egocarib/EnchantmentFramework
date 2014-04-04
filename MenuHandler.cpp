4b4f09a6f5658814a94cd92241efd70c24c37d90
egocarib <egocaribx@gmail.com>
2014-04-03 23:11:59 -0400

    Beginning clean-up...

--- MenuHandler.cpp
+++ MenuHandler.cpp
@@ -11,28 +11,21 @@ BSFixedString		EnchantMenuString;
 LocalMenuHandler	g_menuHandler;
 ICriticalSection	conditionEditLock;
 
-class ConditionedWeaponEnchantments : public EnchantmentInfoLib::EnchantmentConditionMap
+class ConditionedWeaponEnchantments : public EnchantmentConditionMap
 {
   public:
 	bool Accept(EnchantmentItem* pEnch)
 	{
-		//_MESSAGE("Accepting enchantment 0x%08X (unk08: %u unk0C: %u unk10: %u unk14: %u unk18: %u)", pEnch->formID, pEnch->unk08, pEnch->unk0C, pEnch->unk10, pEnch->unk14, pEnch->unk18);
 		if (pEnch->data.unk10 == 0x01) //delivery type: 'Contact' (weapon enchantment)
 		{
-			_MESSAGE("    Weapon Enchantment detected.... %s [%08X]", (DYNAMIC_CAST(pEnch, EnchantmentItem, TESFullName))->name.data, pEnch->formID);
 			ConditionedEffectMap conditionedEffects;
 			for (UInt32 i = 0; i < pEnch->effectItemList.count; ++i)
 			{
 				MagicItem::EffectItem* pEffect = NULL;
 				pEnch->effectItemList.GetNthItem(i, pEffect);
-				_MESSAGE("          Checking EffectItem #%u  (%s [%08X])", i + 1, (pEffect) ? (DYNAMIC_CAST(pEffect->mgef, EffectSetting, TESFullName))->name.data : "INVALID", (pEffect) ? pEffect->mgef->formID : 0);
 				if (pEffect && pEffect->condition)
-				{
-					_MESSAGE("          Has Condition!   <-----------------------------------------------------------------");
-					conditionedEffects[pEffect] = pEffect->condition;
-				}
+					conditionedEffects[pEffect] = pEffect->condition; // 'condition' renamed from 'unk14'
 			}
-			_MESSAGE("          Enchantment has %u conditioned effects.", conditionedEffects.size());
 			if (!conditionedEffects.empty()) //add to EnchantmentConditionMap
 				(*this)[pEnch] = conditionedEffects;
 		}
@@ -40,7 +33,7 @@ class ConditionedWeaponEnchantments : public EnchantmentInfoLib::EnchantmentCond
 	}
 
 	ConditionedWeaponEnchantments() {}
-	ConditionedWeaponEnchantments(EnchantmentDataHandler* enchantments) { enchantments->Visit(this); }
+	//ConditionedWeaponEnchantments(EnchantmentDataHandler* enchantments) { enchantments->Visit(this); }
 };
 
 ConditionedWeaponEnchantments		cWeaponEnchants;
@@ -48,50 +41,46 @@ ConditionedWeaponEnchantments		cWeaponEnchants;
 
 void MenuHandler::InitializeMenuMonitor()
 {
-	_MESSAGE("Initializing Menu Monitor...");
-	static EnchantmentDataHandler	eData;
-	cWeaponEnchants = ConditionedWeaponEnchantments(&eData);
-	LocalMenuManager = MenuManager::GetSingleton();
-	EnchantMenuString = UIStringHolder::GetSingleton()->craftingMenu;
-	LocalMenuManager->MenuOpenCloseEventDispatcher()->AddEventSink(&g_menuHandler);
+	static bool firstLoad = true;
+	if (firstLoad)
+	{
+		firstLoad = false;
+		cWeaponEnchants = ConditionedWeaponEnchantments();
+		EnchantmentDataHandler::Visit(&cWeaponEnchants);
+		LocalMenuManager = MenuManager::GetSingleton();
+		EnchantMenuString = UIStringHolder::GetSingleton()->craftingMenu;
+		LocalMenuManager->MenuOpenCloseEventDispatcher()->AddEventSink(&g_menuHandler);
+	}
 }
 
 EventResult LocalMenuHandler::ReceiveEvent(MenuOpenCloseEvent * evn, EventDispatcher<MenuOpenCloseEvent> * dispatcher)
 {
-	//_MESSAGE("MENU EVENT TRIGGERED");
-
 	if (evn->menuName.data != EnchantMenuString.data)
 		return kEvent_Continue;
 
-	conditionEditLock.Enter(); //technically this still probably isn't threadsafe, if threads pile up and enter
-						//in wrong order - however, this is also pretty fast so I don't see how the user could
-						//conceivably open/close the crafting menu fast enough to generate any problems..
+	conditionEditLock.Enter();
+	//technically this still probably isn't threadsafe, if threads pile up and enter
+	//in wrong order - however, this is also pretty fast so I don't see how the user could
+	//conceivably open/close the crafting menu fast enough to generate any problems..
 
-	if (evn->opening)
+	if (evn->opening) //detach conditions from weapon enchantments
 	{
-		_MESSAGE("ConditionedWeaponEnchantments Map Size: %u", cWeaponEnchants.size());
-		_MESSAGE("    Detaching Condition Pointers...");
-
-		//detach conditions from weapon enchantments
 		for (ConditionedWeaponEnchantments::iterator enchIt = cWeaponEnchants.begin(); enchIt != cWeaponEnchants.end(); ++enchIt)
 			for(ConditionedEffectMap::iterator effectIt = enchIt->second.begin(); effectIt != enchIt->second.end(); ++effectIt)
 				effectIt->first->condition = NULL;
 	}
-	else
+	else //re-attach conditions
 	{
-		//_MESSAGE("ConditionedWeaponEnchantments Map Size: %u", cWeaponEnchants.size());
-		_MESSAGE("    Re-attaching Condition Pointers...");
-
-		//re-attach conditions
 		for (ConditionedWeaponEnchantments::iterator enchIt = cWeaponEnchants.begin(); enchIt != cWeaponEnchants.end(); ++enchIt)
-		{
 			for(ConditionedEffectMap::iterator effectIt = enchIt->second.begin(); effectIt != enchIt->second.end(); ++effectIt)
 				effectIt->first->condition = effectIt->second;
-			DumpEnchantmentData(enchIt->first); //DEBUG
-		}
 	}
 
 	conditionEditLock.Leave();
+
+	if (!evn->opening)
+		//ConditionalizeNewPlayerEnchantments() //vanilla bug results in base enchantment conditions being stripped from all player-enchanted items
+		//NEED TO REPLACE THIS FUNCTION
 
 	return kEvent_Continue;
 }
--- MenuHandler.h
+++ MenuHandler.h
@@ -2,6 +2,10 @@
 
 #include "skse/GameMenus.h"
 #include "skse/GameEvents.h"
+#include "skse/GameObjects.h"
+
+typedef std::map <MagicItem::EffectItem*, Condition*>		ConditionedEffectMap;
+typedef std::map <EnchantmentItem*, ConditionedEffectMap>	EnchantmentConditionMap;
 
 class LocalMenuHandler : public BSTEventSink <MenuOpenCloseEvent>
 {
