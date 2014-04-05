caf3c80f8ac168f0e52845bcbd606cf626894c31
egocarib <egocaribx@gmail.com>
2014-04-05 01:45:38 -0400

    Migrated class templates into header

--- MenuHandler.h
+++ MenuHandler.h
@@ -7,17 +7,32 @@
 typedef std::map <MagicItem::EffectItem*, Condition*>		ConditionedEffectMap;
 typedef std::map <EnchantmentItem*, ConditionedEffectMap>	EnchantmentConditionMap;
 
+
 class LocalMenuHandler : public BSTEventSink <MenuOpenCloseEvent>
 {
 public:
 	virtual EventResult		ReceiveEvent(MenuOpenCloseEvent * evn, EventDispatcher<MenuOpenCloseEvent> * dispatcher);
 };
 
-extern MenuManager*			LocalMenuManager;
-extern BSFixedString		EnchantMenuString;
-extern LocalMenuHandler		g_menuHandler;
 
-namespace MenuHandler
+class ConditionedWeaponEnchantments : public EnchantmentConditionMap
 {
+  public:
+	bool Accept(EnchantmentItem* pEnch);
+};
+
+
+typedef ConditionedWeaponEnchantments::iterator		CndEnchantIter;
+typedef ConditionedEffectMap::iterator				CndEffectIter;
+
+
+class MenuCore //Statics that don't need to change between loads in the same play session
+{
+public:
 	void InitializeMenuMonitor();
-}
+
+	static LocalMenuHandler					thisMenu;
+	static BSFixedString					enchantMenuString;
+	static ConditionedWeaponEnchantments	cWeaponEnchants;
+};
+
